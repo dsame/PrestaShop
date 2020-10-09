75c3fafec0a6bb75f9dcd442cde4dea0d08fd176
Sergey Dolin <v-sedoli@micorosoft.com>
2020-10-09 15:36:48 +0500

    l.php

--- l.php
+++ l.php
@@ -1,5 +1,5 @@
 <?php
 set_include_path ('/home/runner/work/PrestaShop/PrestaShop/vendor/pear/pear_exception:/home/runner/work/PrestaShop/PrestaShop/vendor/pear/console_getopt:/home/runner/work/PrestaShop/PrestaShop/vendor/pear/pear-core-minimal/src:/home/runner/work/PrestaShop/PrestaShop/vendor/pear/archive_tar:/home/runner/work/PrestaShop/PrestaShop/vendor/pear/pear_exception:/home/runner/work/PrestaShop/PrestaShop/vendor/pear/console_getopt:/home/runner/work/PrestaShop/PrestaShop/vendor/pear/pear-core-minimal/src:/home/runner/work/PrestaShop/PrestaShop/vendor/pear/archive_tar:.:/usr/share/php');
-require('/xhome/runner/work/PrestaShop/PrestaShop/classes/Attribute.php');
+#require('/home/runner/work/PrestaShop/PrestaShop/classes/Attribute.php');
 # require('classes/xAttribute.php');
 ?>
