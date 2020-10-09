11d9d71114bc9c89de678dd1382011d9c066a196
Sergey Dolin <v-sedoli@micorosoft.com>
2020-10-09 15:38:08 +0500

    remove comments Fri Oct  9 15:38:08 +05 2020

--- classes/Attribute.php
+++ classes/Attribute.php
@@ -1,49 +1,12 @@
 <?php
-/**
- * Copyright since 2007 PrestaShop SA and Contributors
- * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.md.
- * It is also available through the world-wide-web at this URL:
- * https://opensource.org/licenses/OSL-3.0
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@prestashop.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to https://devdocs.prestashop.com/ for more information.
- *
- * @author    PrestaShop SA and Contributors <contact@prestashop.com>
- * @copyright Since 2007 PrestaShop SA and Contributors
- * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
- */
-
-/**
- * Class AttributeCore.
- */
 class AttributeCore extends ObjectModel
 {
-    /** @var int Group id which attribute belongs */
     public $id_attribute_group;
-
-    /** @var string Name */
     public $name;
-    /** @var string */
     public $color;
-    /** @var int */
     public $position;
-    /** @todo Find type */
     public $default;
 
-    /**
-     * @see ObjectModel::$definition
-     */
     public static $definition = [
         'table' => 'attribute',
         'primary' => 'id_attribute',
@@ -58,10 +21,8 @@ class AttributeCore extends ObjectModel
         ],
     ];
 
-    /** @var string */
     protected $image_dir = _PS_COL_IMG_DIR_;
 
-    /** @var array Web service parameters */
     protected $webserviceParameters = [
         'objectsNodeName' => 'product_option_values',
         'objectNodeName' => 'product_option_value',
@@ -70,22 +31,12 @@ class AttributeCore extends ObjectModel
         ],
     ];
 
-    /**
-     * AttributeCore constructor.
-     *
-     * @param int|null $id Attribute ID
-     * @param int|null $idLang Language ID
-     * @param int|null $idShop Shop ID
-     */
     public function __construct($id = null, $idLang = null, $idShop = null)
     {
         parent::__construct($id, $idLang, $idShop);
         $this->image_dir = _PS_COL_IMG_DIR_;
     }
 
-    /**
-     * @see ObjectModel::delete()
-     */
     public function delete()
     {
         if (!$this->hasMultishopEntries() || Shop::getContext() == Shop::CONTEXT_ALL) {
@@ -141,17 +92,6 @@ class AttributeCore extends ObjectModel
         return $return;
     }
 
-    /**
-     * Adds current Attribute as a new Object to the database.
-     *
-     * @param bool $autoDate Automatically set `date_upd` and `date_add` column
-     * @param bool $nullValues Whether we want to use NULL values instead of empty quotes values
-     *
-     * @return bool Whether the Attribute has been successfully added
-     *
-     * @throws PrestaShopDatabaseException
-     * @throws PrestaShopException
-     */
     public function add($autoDate = true, $nullValues = false)
     {
         if ($this->position <= 0) {
@@ -167,14 +107,6 @@ class AttributeCore extends ObjectModel
         return $return;
     }
 
-    /**
-     * Get all attributes for a given language.
-     *
-     * @param int $idLang Language ID
-     * @param bool $notNull Get only not null fields if true
-     *
-     * @return array Attributes
-     */
     public static function getAttributes($idLang, $notNull = false)
     {
         if (!Combination::isFeatureActive()) {
@@ -197,15 +129,6 @@ class AttributeCore extends ObjectModel
 		');
     }
 
-    /**
-     * Check if the given name is an Attribute within the given AttributeGroup.
-     *
-     * @param int $idAttributeGroup AttributeGroup
-     * @param string $name Attribute name
-     * @param int $idLang Language ID
-     *
-     * @return array|bool
-     */
     public static function isAttribute($idAttributeGroup, $name, $idLang)
     {
         if (!Combination::isFeatureActive()) {
@@ -230,16 +153,6 @@ class AttributeCore extends ObjectModel
         return (int) $result > 0;
     }
 
-    /**
-     * Get quantity for a given attribute combination
-     * Check if quantity is enough to serve the customer.
-     *
-     * @param int $idProductAttribute Product attribute combination id
-     * @param int $qty Quantity needed
-     * @param Shop $shop Shop
-     *
-     * @return bool Quantity is available or not
-     */
     public static function checkAttributeQty($idProductAttribute, $qty, Shop $shop = null)
     {
         if (!$shop) {
@@ -251,11 +164,6 @@ class AttributeCore extends ObjectModel
         return $result && $qty <= $result;
     }
 
-    /**
-     * Return true if the Attribute is a color.
-     *
-     * @return bool Color is the attribute type
-     */
     public function isColorAttribute()
     {
         if (!Db::getInstance()->getRow('
@@ -272,13 +180,6 @@ class AttributeCore extends ObjectModel
         return Db::getInstance()->numRows();
     }
 
-    /**
-     * Get minimal quantity for product with attributes quantity.
-     *
-     * @param int $idProductAttribute Product Attribute ID
-     *
-     * @return mixed Minimal quantity or false if no result
-     */
     public static function getAttributeMinimalQty($idProductAttribute)
     {
         $minimalQuantity = Db::getInstance()->getValue(
@@ -296,14 +197,6 @@ class AttributeCore extends ObjectModel
         return false;
     }
 
-    /**
-     * Move an attribute inside its group.
-     *
-     * @param bool $direction Up (1) or Down (0)
-     * @param int $position Current position of the attribute
-     *
-     * @return bool Update result
-     */
     public function updatePosition($direction, $position)
     {
         if (!$idAttributeGroup = (int) Tools::getValue('id_attribute_group')) {
@@ -355,15 +248,6 @@ class AttributeCore extends ObjectModel
         return $res1 && $res2;
     }
 
-    /**
-     * Reorder the attribute position within the Attribute group.
-     * Call this method after deleting an attribute from a group.
-     *
-     * @param int $idAttributeGroup Attribute group ID
-     * @param bool $useLastAttribute
-     *
-     * @return bool Whether the result was successfully updated
-     */
     public function cleanPositions($idAttributeGroup, $useLastAttribute = true)
     {
         Db::getInstance()->execute('SET @i = -1', false);
@@ -378,16 +262,6 @@ class AttributeCore extends ObjectModel
         return Db::getInstance()->execute($sql);
     }
 
-    /**
-     * get highest position.
-     *
-     * Get the highest attribute position from a group attribute
-     *
-     * @param int $idAttributeGroup AttributeGroup ID
-     *
-     * @return int $position Position
-     * @todo: Shouldn't this be called getHighestPosition instead?
-     */
     public static function getHigherPosition($idAttributeGroup)
     {
         $sql = 'SELECT MAX(`position`)
